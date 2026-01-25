51fd99435561478e9f57014fecbe212d6868c26f
Martin Poole <martin_poole@hotmail.com>
2026-01-25 21:26:33 +0000

    adding blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -19,9 +19,6 @@ export async function generateMetadata({
     };
   }
 
-  const siteUrl =
-    process.env.NEXT_PUBLIC_SITE_URL || "https://www.martinpoole.cv";
-
   return {
     title: `${post.title} | Martin Poole`,
     description: post.excerpt,
@@ -35,7 +32,7 @@ export async function generateMetadata({
       images: post.coverImage
         ? [
             {
-              url: `${siteUrl}${post.coverImage}`,
+              url: post.coverImage,
               width: 1200,
               height: 630,
               alt: post.title,
@@ -47,7 +44,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title: post.title,
       description: post.excerpt,
-      images: post.coverImage ? [`${siteUrl}${post.coverImage}`] : [],
+      images: post.coverImage ? [post.coverImage] : [],
     },
   };
 }
